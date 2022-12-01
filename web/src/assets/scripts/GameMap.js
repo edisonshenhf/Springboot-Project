4b96d9eef2e5a52a6711b860f36ff85deeba57ae
Edisonshenhf <a492019098@gmail.com>
2022-12-01 12:35:09 -0700

    record & ranklist implement

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,6 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
-import { Snake } from "./Snake";
-import { Wall } from './Wall';
+import { Wall } from "./Wall";
+import { Snake } from './Snake';
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent, store) {
@@ -10,6 +10,7 @@ export class GameMap extends AcGameObject {
         this.parent = parent;
         this.store = store;
         this.L = 0;
+
         this.rows = 13;
         this.cols = 14;
 
@@ -19,12 +20,12 @@ export class GameMap extends AcGameObject {
         this.snakes = [
             new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
             new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
-        ]
-
+        ];
     }
 
     create_walls() {
         const g = this.store.state.pk.gamemap;
+
         for (let r = 0; r < this.rows; r++) {
             for (let c = 0; c < this.cols; c++) {
                 if (g[r][c]) {
@@ -32,31 +33,52 @@ export class GameMap extends AcGameObject {
                 }
             }
         }
-        return true;
-
     }
 
     add_listening_events() {
-        this.ctx.canvas.focus();
+        console.log(this.store.state.record);
 
-        this.ctx.canvas.addEventListener("keydown", e => {
-            let d = -1;
-            if (e.key === 'w') d = 0;
-            else if (e.key === 'd') d = 1;
-            else if (e.key === 's') d = 2;
-            else if (e.key === 'a') d = 3;
+        if (this.store.state.record.is_record) {
+            let k = 0;
 
-            if (d >= 0) {
-                this.store.state.pk.socket.send(JSON.stringify({
-                    event: "move",
-                    direction: d,
+            const a_steps = this.store.state.record.a_steps;
+            const b_steps = this.store.state.record.b_steps;
+            const loser = this.store.state.record.record_loser;
+            const [snake0, snake1] = this.snakes;
+            const interval_id = setInterval(() => {
+                if (k >= a_steps.length - 1) {
+                    if (loser === "all" || loser === "A") {
+                        snake0.status = "die";
+                    }
+                    if (loser === "all" || loser === "B") {
+                        snake1.status = "die";
+                    }
+                    clearInterval(interval_id);
+                } else {
+                    snake0.set_direction(parseInt(a_steps[k]));
+                    snake1.set_direction(parseInt(b_steps[k]));
+                }
+                k++;
+            }, 300);
+        } else {
+            this.ctx.canvas.focus();
 
-                }));
-            }
-        });
+            this.ctx.canvas.addEventListener("keydown", e => {
+                let d = -1;
+                if (e.key === 'w') d = 0;
+                else if (e.key === 'd') d = 1;
+                else if (e.key === 's') d = 2;
+                else if (e.key === 'a') d = 3;
+
+                if (d >= 0) {
+                    this.store.state.pk.socket.send(JSON.stringify({
+                        event: "move",
+                        direction: d,
+                    }));
+                }
+            });
+        }
     }
-
-
 
     start() {
         this.create_walls();
@@ -65,7 +87,7 @@ export class GameMap extends AcGameObject {
     }
 
     update_size() {
-        this.L = Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows);
+        this.L = parseInt(Math.min(this.parent.clientWidth / this.cols, this.parent.clientHeight / this.rows));
         this.ctx.canvas.width = this.L * this.cols;
         this.ctx.canvas.height = this.L * this.rows;
     }
@@ -78,30 +100,29 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
-
-    next_step() {//让两条蛇进入下一回合
+    next_step() {  // 让两条蛇进入下一回合
         for (const snake of this.snakes) {
             snake.next_step();
         }
     }
 
-    check_valid(cell) {//检测目标位置是否合法
+    check_valid(cell) {  // 检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
         for (const wall of this.walls) {
-            if (wall.r === cell.r && wall.c === cell.c) {
+            if (wall.r === cell.r && wall.c === cell.c)
                 return false;
-            }
         }
+
         for (const snake of this.snakes) {
             let k = snake.cells.length;
-            if (!snake.check_tail_increasing()) {//蛇尾会前进的时候不需要判断
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
                 k--;
             }
             for (let i = 0; i < k; i++) {
-                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
                     return false;
-                }
             }
         }
+
         return true;
     }
 
